b26f8bbfabb72ffd867bdf2e2c4ccb9dee1919df
yuchin <racingf1@gmail.com>
2025-08-27 17:30:48 +0800

    feat: Add product data and display to About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,20 @@
 <template>
-<!-- <div id="app" class="container py-4"> -->
+  <!-- <div id="app" class="container py-4"> -->
   <div class="row">
     <!-- 商品列表區 -->
     <div class="col-md-8">
       <h2 class="mb-3">商品列表</h2>
       <div class="row">
-        <div class="col-md-4 mb-4">
+        <div class="col-md-4 mb-4" v-for="p in products" :key="p.id">
           <div class="card h-100">
-            <img src="https://images.unsplash.com/photo-1546435770-a3e426bf472b?q=80&amp;w=2065&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top">
+            <img
+              :src="p.imgURL"
+              :alt="p.title"
+              class="card-img-top">
             <div class="card-body">
-              <h5 class="card-title">耳罩式藍牙耳機</h5>
-              <p class="card-text">舒適配戴，支援降噪技術</p>
-              <p class="fw-bold text-primary">$ 2490</p>
+              <h5 class="card-title">{{ p.title }}</h5>
+              <p class="card-text">{{ p.content }}</p>
+              <p class="fw-bold text-primary">{{ p.price }}</p>
               <button class="btn btn-success w-100"> 加入購物車 </button>
             </div>
           </div>
@@ -46,7 +49,7 @@
       </div>
     </div>
   </div>
-<!-- </div> -->
+  <!-- </div> -->
 </template>
 
 <style scoped>
@@ -58,9 +61,58 @@ body {
   height: 150px;
   object-fit: cover;
 }  */
-
 </style>
 
 <script setup>
+import { ref,provide } from 'vue';
+// import { useRouter } from 'vue-router';
+// import axios from 'axios';
+
+// const router = useRouter();
+// 商品資料
+const products = ref([
+  {
+    id: 1,
+    imgURL:
+      'https://images.unsplash.com/photo-1546435770-a3e426bf472b?q=80&amp;w=2065&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: '耳罩式藍牙耳機',
+    content: '舒適配戴，支援降噪技術',
+    price: 2490,
+  },
+  {
+    id: 2,
+    imgURL:
+      'https://images.unsplash.com/photo-1524678606370-a47ad25cb82a?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: '耳罩式彩虹耳機',
+    content: '舒適配戴，支援降噪技術',
+    price: 1380,
+  },
+  {
+    id: 3,
+    imgURL:
+      'https://images.unsplash.com/photo-1636099184052-ada1f331132c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: '時尚藍芽耳機',
+    content: '舒適配戴，支援降噪技術',
+    price: 7990,
+  },
+  {
+    id: 4,
+    imgURL:
+      'https://images.unsplash.com/photo-1672292086075-aa75f03ed2a8?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: '機械式鍵盤',
+    content: '機械鍵盤，打字手感極佳',
+    price: 1890,
+  },
+  {
+    id: 5,
+    imgURL:
+      'https://images.unsplash.com/photo-1586349906319-48d20e9d17e5?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: '無線滑鼠',
+    content: '靜音按鈕設計，長效電池',
+    price: 890,
+  },
+])
+
+
 
 </script>
